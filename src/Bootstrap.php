994aab2da30c9a99b9ecd9f96f74a24784e0a29b
Konrad Fedorczyk <fedek6@gmail.com>
2021-04-22 16:45:50 +0200

    Add comments to bootstrap class

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,20 +4,42 @@ declare(strict_types=1);
 
 namespace Fedek6\WpMPB;
 
+/**
+ * Main plugin bootstrap class.
+ * 
+ * @package     wp-modern-plugin-boilerplate
+ * @subpackage  core
+ * @version     1.0.0
+ * @author      Konrad Fedorczyk <contact@realhe.ro>
+ */
 class Bootstrap
 {
+    /** @var array $component A pair of component' id & class. */
     private $components = [];
 
+    /** @var string $version Semantic version of a component. */
     private $version;
 
+    /** @var string $assetsUrl Absolute assets URL. */
     private $assetsUrl;
 
+    /** @var string $pluginName Simple unique plugin identificator. */
     private $pluginName;
 
+    /** @var string $pluginPath Absolute plugin path. */
     private $pluginPath;
 
-    private $registeredComponents = [];
+    /** @var array $componentInstance A pair of component' id & instance. */
+    private $componentInstances = [];
 
+    /**
+     * Constructor.
+     * 
+     * @param  string  $pluginName  Simple unique plugin identificator.
+     * @param  string  $assetsUrl   Absolute assets URL.
+     * @param  string  $pluginPath  Absolute plugin path.
+     * @param  string  $version     Semantic version of a component.
+     */
     public function __construct(
         string $pluginName,
         string $assetsUrl,
@@ -30,6 +52,13 @@ class Bootstrap
         $this->version = $version;
     }
 
+    /**
+     * Register component. 
+     * 
+     * @param  string  $name       Unique component name.
+     * @param  string  $component  Component class (with namespace).
+     * @throws \Error
+     */
     public function registerComponent(string $name, string $component)
     {
         if (class_exists($component)) {
@@ -39,17 +68,22 @@ class Bootstrap
         }
     }
 
+    /**
+     * Main exposed run method.
+     */
     public function run()
     {
         foreach ($this->components as $name => $component) {
-            $this->registeredComponents[$name] = new $component(
+            // Create instance.
+            $this->componentInstances[$name] = new $component(
                 $this->pluginName,
                 $this->assetsUrl,
                 $this->pluginPath,
                 $this->version
             );
 
-            $this->registeredComponents[$name]->run();
+            // Run.
+            $this->componentInstances[$name]->run();
         }
     }
 }
