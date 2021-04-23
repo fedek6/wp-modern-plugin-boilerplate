243e6561e80d1bce7415b9c4dbbf3a5d21347f2d
Konrad Fedorczyk <fedek6@gmail.com>
2021-04-23 13:39:21 +0200

    Fix eslint config location

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
     project: "./tsconfig.json",
+    tsconfigRootDir: __dirname
   },
   rules: {
     "linebreak-style": "off",
